8eb29be6a1f9a3d8c14e47291b784ddbe8ba6341
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-08-09 19:43:08 +0700

    A

---
+++ app/src/main/java/khoapham/ptp/phamtanphat/retrofit210052019/MainActivity.java
@@ -0,0 +1,14 @@
+package khoapham.ptp.phamtanphat.retrofit210052019;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
